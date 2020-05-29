ab215d94317814e8a61876162b0d3e561a7214bd
Kuan-Chun (Jean) Lin <jeanlinkcl@gmail.com>
2020-05-29 18:06:30 -0400

    add connection to socket.io

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
-
+const socketIO = require('socket.io');
 
 const public_path = path.join(__dirname, '/../public');
 const port = process.env.PORT || 8080;
-var app = express();
+let app = express();
+let server = http.createServer(app); // create our own server
+let io = socketIO(server);
 
 app.use(express.static(public_path));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log("A new user just connected");
+
+    socket.on('disconnect', () => {
+        console.log("User is disconnected");
+    });
+
+});
+
+
+
+server.listen(port, () => {
     console.log(`Server is out on port ${port}`);
-})
+});
+
