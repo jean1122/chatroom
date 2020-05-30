7315a7e80b4e173007c3faa5fa59358a35a66739
Kuan-Chun (Jean) Lin <jeanlinkcl@gmail.com>
2020-05-30 16:39:27 -0400

    add custom broadcast emits

--- server/server.js
+++ server/server.js
@@ -14,13 +14,34 @@ app.use(express.static(public_path));
 io.on('connection', (socket) => {
     console.log("A new user just connected");
 
+    socket.emit('newMessage', {
+        from: "Admin",
+        text: "Welcome to the chat!",
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: "Admin",
+        text: "New user joined",
+        createdAt: new Date().getTime()
+    }); // send to everyone else except me
+
+    socket.on('createMessage', (message) => {
+        console.log("CreateMessage", message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+
+
+    });
+
     socket.on('disconnect', () => {
         console.log("User is disconnected");
     });
 
 });
-
-
 
 server.listen(port, () => {
     console.log(`Server is out on port ${port}`);
